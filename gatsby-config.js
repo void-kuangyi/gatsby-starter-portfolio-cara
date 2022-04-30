cf63a256cf5709898032586e87c2d26392857d19
Kuangyi <kuanyi@Kuanyis-MacBook-Pro.local>
2022-04-30 23:01:55 +0200

    add basic content and shadowing the theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,11 +7,11 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
+    siteTitle: `Xing Kuangyi`,
+    siteTitleAlt: `Xing Kuangyi's portfolio site`,
+    siteHeadline: `Xing Kuangyi's portfolio site`,
     siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteDescription: `Portfolio website for Xing Kuangyi`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
     author: `@lekoarts_de`,
---
+++ src/@lekoarts/gatsby-theme-cara/components/project-card.tsx
@@ -0,0 +1,56 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui"
+import { Badge } from 'styled-badge-component';
+
+type ProjectCardProps = {
+  link: string
+  title: string,
+  status: string,
+  children: React.ReactNode
+  bg: string
+}
+
+const ProjectCard = ({ link, title, status, children, bg }: ProjectCardProps) => (
+  <a
+    href={link}
+    target="_blank"
+    rel="noreferrer noopener"
+    sx={{
+      width: `100%`,
+      boxShadow: `lg`,
+      position: `relative`,
+      textDecoration: `none`,
+      borderRadius: `lg`,
+      px: 4,
+      py: [4, 5],
+      color: `white`,
+      background: bg || `none`,
+      transition: `all 0.4s cubic-bezier(0.175, 0.885, 0.32, 1.275) !important`,
+      "&:hover": {
+        color: `white !important`,
+        transform: `translateY(-5px)`,
+        boxShadow: `xl`,
+      },
+    }}
+  >
+    <div sx={{ margin: `-20px 0px 30px 0px`}}>
+      {
+        status === "In progress" ? <Badge pill warning>{status}</Badge> : <Badge pill success>{status}</Badge>
+      }
+    </div>
+    <div sx={{ opacity: 0.85, textShadow: `0 2px 10px rgba(0, 0, 0, 0.3)` }}>{children}</div>
+    <div
+      sx={{
+        letterSpacing: `wide`,
+        pt: 4,
+        fontSize: [4, 5],
+        fontWeight: `medium`,
+        lineHeight: 1,
+      }}
+    >
+      {title}
+    </div>
+  </a>
+)
+
+export default ProjectCard
