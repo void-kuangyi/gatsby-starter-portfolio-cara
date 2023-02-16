562d91a83fce080d56e1f1923e9259f6b7c1b9bb
kuangyi <xingkuangyi@gmail.com>
2023-02-16 16:45:09 +0100

    add jenga

---
+++ src/pages/jenga.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import Layout from "@lekoarts/gatsby-theme-cara/src/components/layout";
+import Inner from "@lekoarts/gatsby-theme-cara/src/elements/inner";
+import JengaMDX from "../@lekoarts/gatsby-theme-cara/components/jenga";
+
+const Jenga = () => (
+  <Layout>
+    <Inner>
+      <JengaMDX />
+    </Inner>
+  </Layout>
+);
+
+export default Jenga;
---
+++ src/pages/wind.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import Layout from "@lekoarts/gatsby-theme-cara/src/components/layout";
+import Inner from "@lekoarts/gatsby-theme-cara/src/elements/inner";
+import WindMDX from "../@lekoarts/gatsby-theme-cara/components/wind";
+
+const Wind = () => (
+  <Layout>
+    <Inner>
+      <WindMDX />
+    </Inner>
+  </Layout>
+);
+
+export default Wind;
