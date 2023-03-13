010c42be262a143d6fe130b96778c13e5c518d98
kuangyi <xingkuangyi@gmail.com>
2023-03-13 22:12:47 +0100

    update colour

--- src/@lekoarts/gatsby-theme-cara/gatsby-plugin-theme-ui/index.js
+++ src/@lekoarts/gatsby-theme-cara/gatsby-plugin-theme-ui/index.js
@@ -11,7 +11,7 @@ const theme = merge(tailwind, {
     secondary: tailwind.colors.indigo[6],
     text: tailwind.colors.gray[3],
     heading: tailwind.colors.white,
-    background: `#141821`,
+    background: tailwind.colors.gray[1],
     divider: tailwind.colors.gray[8],
     textMuted: tailwind.colors.gray[5],
     icon_brightest: tailwind.colors.white,
