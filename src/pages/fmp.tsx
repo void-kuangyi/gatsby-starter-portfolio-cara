de52e3a4586f3f781c2713c82fe440eaec1c2099
kuangyi <xingkuangyi@gmail.com>
2023-05-14 17:52:23 +0200

    add fmp

---
+++ src/pages/fmp.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import Layout from "@lekoarts/gatsby-theme-cara/src/components/layout";
+import Inner from "@lekoarts/gatsby-theme-cara/src/elements/inner";
+import FMPContent from "../@lekoarts/gatsby-theme-cara/components/fmp";
+
+const FMP = () => (
+  <Layout>
+    <Inner>
+      <FMPContent />
+    </Inner>
+  </Layout>
+);
+
+export default FMP;
