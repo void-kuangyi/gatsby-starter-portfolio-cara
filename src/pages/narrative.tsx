3ab4644c47f32be1157611731b9f58b83e2d69ea
kuangyi <xingkuangyi@gmail.com>
2023-06-21 22:00:17 +0200

    add the narrative tool

---
+++ src/pages/narrative.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import Layout from "@lekoarts/gatsby-theme-cara/src/components/layout";
+import Inner from "@lekoarts/gatsby-theme-cara/src/elements/inner";
+import NarrativeContent from "../@lekoarts/gatsby-theme-cara/components/narrative";
+
+const Narrative = () => (
+  <Layout>
+    <Inner>
+      <NarrativeContent />
+    </Inner>
+  </Layout>
+);
+
+export default Narrative;
