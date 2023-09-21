1f3d517d955e618aaffecd6ac5ada7e3477bfe53
kuangyi <xingkuangyi@gmail.com>
2023-09-21 13:10:40 +0200

    remove texts

--- src/@lekoarts/gatsby-theme-cara/components/project-card.tsx
+++ src/@lekoarts/gatsby-theme-cara/components/project-card.tsx
@@ -26,34 +26,37 @@ const ProjectCard = ({
   bg,
   keywords,
   textColor,
-}: ProjectCardProps) => (
-  <a
-    href={link}
-    rel="noreferrer noopener"
-    className={className}
-    sx={{
-      display: `flex`,
-      flexDirection: `column`,
-      justifyContent: `space-between`,
-      width: `100%`,
-      boxShadow: `lg`,
-      position: `relative`,
-      textDecoration: `none`,
-      borderRadius: `lg`,
-      px: 4,
-      py: [4, 5],
-      color: textColor || `white`,
-      background: bg || `none`,
-      backgroundSize: `cover`,
-      transition: `all 0.4s cubic-bezier(0.175, 0.885, 0.32, 1.275) !important`,
-      "&:hover": {
-        color: textColor || `white !important`,
-        transform: `translateY(-5px)`,
-        boxShadow: `xl`,
-      },
-    }}
-  >
-    <div
+}: ProjectCardProps) => {
+  const modifiedTitle = title.replace(/ /g, "\n");
+  console.log(modifiedTitle);
+  return (
+    <a
+      href={link}
+      rel="noreferrer noopener"
+      className={className}
+      sx={{
+        display: `flex`,
+        flexDirection: `column`,
+        justifyContent: `flex-end`,
+        width: `100%`,
+        boxShadow: `lg`,
+        position: `relative`,
+        textDecoration: `none`,
+        borderRadius: `lg`,
+        px: 4,
+        py: [4, 3],
+        color: textColor || `white`,
+        background: bg || `none`,
+        backgroundSize: `cover`,
+        transition: `all 0.4s cubic-bezier(0.175, 0.885, 0.32, 1.275) !important`,
+        "&:hover": {
+          color: textColor || `white !important`,
+          transform: `translateY(-5px)`,
+          boxShadow: `xl`,
+        },
+      }}
+    >
+      {/* <div
       sx={{
         margin: `-20px 0px 30px 0px`,
         display: `flex`,
@@ -77,43 +80,37 @@ const ProjectCard = ({
           {collab}
         </Badge>
       )}
-    </div>
-    <div>
-      {textColor == "black" ? (
+    </div> */}
+      <div>
         <div
           sx={{
-            fontWeight: 600,
-            fontSize: 1,
+            letterSpacing: `wide`,
+            pt: 4,
+            fontSize: [4, 5],
+            fontWeight: `medium`,
+            lineHeight: 1,
+            fontFamily: "scandia-line-web-stencil, sans-serif",
+            fontStyle: "normal",
+            marginTop: "200px",
+            marginBottom: "20px",
           }}
         >
-          {children}
+          {modifiedTitle}
         </div>
-      ) : (
-        <div
-          sx={{
-            textShadow: `1px 2px 7px rgba(0, 0, 0)`,
-            fontWeight: 600,
-            fontSize: 1,
-          }}
-        >
-          {children}
-        </div>
-      )}
-      <div
-        sx={{
-          letterSpacing: `wide`,
-          pt: 4,
-          fontSize: [4, 5],
-          fontWeight: `medium`,
-          lineHeight: 1,
-          fontFamily: "scandia-line-web-stencil, sans-serif",
-          fontStyle: "normal",
-        }}
-      >
-        {title}
+        {children != undefined && (
+          <div
+            sx={{
+              textShadow: textColor != "black" && `1px 2px 7px rgba(0, 0, 0)`,
+              fontWeight: 600,
+              fontSize: 1,
+              marginBottom: "20px",
+            }}
+          >
+            {children}
+          </div>
+        )}
       </div>
-    </div>
-    <div
+      {/* <div
       sx={{
         display: `flex`,
         alignItems: `flex-end`,
@@ -134,8 +131,9 @@ const ProjectCard = ({
           </Badge>
         ))}
       </div>
-    </div>
-  </a>
-);
+    </div> */}
+    </a>
+  );
+};
 
 export default ProjectCard;
