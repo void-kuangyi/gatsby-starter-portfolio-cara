c4e301360586aecd4ef6326cd93b284605fbad72
kuangyi <xingkuangyi@gmail.com>
2023-11-04 18:54:57 +0100

    add a picture

--- src/@lekoarts/gatsby-theme-cara/elements/imagetext.tsx
+++ src/@lekoarts/gatsby-theme-cara/elements/imagetext.tsx
@@ -10,15 +10,15 @@ const ImageText = ({ src, children }: ImageTextProps) => (
   <div
     sx={{
       display: `flex`,
-      "@media only screen and (max-width: 640px)": {
+      "@media only screen and (max-width: 900px)": {
         flexDirection: `column`,
       },
     }}
   >
-    <div sx={{ marginRight: `40px`, flexBasis: `60%` }}>
-      <img src={src} />
+    <div sx={{ flexBasis: `30%`, marginRight: `10px` }}>
+      <img sx={{ width: `100%`, height: `auto` }} src={src} />
     </div>
-    <div sx={{ flexBasis: `80%` }}>{children}</div>
+    <div sx={{ flexBasis: `50%` }}>{children}</div>
   </div>
 );
 
