8bffa829ea28b183ac39ebfb808de990891b163c
kuangyi <xingkuangyi@gmail.com>
2024-03-04 17:55:14 +0100

    add doyousee project

---
+++ src/pages/doyousee.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import Layout from "@lekoarts/gatsby-theme-cara/src/components/layout";
+import Inner from "../@lekoarts/gatsby-theme-cara/elements/inner";
+import DoyouseeContent from "../@lekoarts/gatsby-theme-cara/sections/doyousee";
+import ContentLayout from "../@lekoarts/gatsby-theme-cara/components/content-layout";
+
+const Doyousee = () => (
+  <ContentLayout>
+    <DoyouseeContent />
+  </ContentLayout>
+);
+
+export default Doyousee;
